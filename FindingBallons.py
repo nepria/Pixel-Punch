db6d325f476309b5d8dbb2d90e5c26625579a3c9
nepria <discord.akhouri@gmail.com>
2023-02-02 17:45:04 +0530

    Bugs Fixed

--- FindingBallons.py
+++ FindingBallons.py
@@ -2,38 +2,34 @@ import cv2
 import numpy as np
 
 def findBallons(img):
-
     # bboxs = []
     img = cropImage(img, 0.1)
     img = preProcess(img)
-    img = findContours(img)
-
-    return img 
+    img = findContours(img) 
+    return img
 
 
 def cropImage(img, cropVal):
-    h, w, c = img.shape
-    img = img[100:h, 0:w]
+    h,w, c = img.shape
+    img = img[int(cropVal*h):h, 0:w]
     return img
 
 def preProcess(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = cv2.GaussianBlur(img, (5, 5), 5, 0)
     img = cv2.Canny(img, 50, 100)
-    Kernel = np.ones((5, 5), np.uint8)
-    img = cv2.dialte(img, kernel)
-
+    kernel = np.ones((5, 5), np.uint8)
+    img = cv2.dilate(img, kernel)
     return img
 
 def findContours(img):
-
-    h, w, c = img.shape
-    imgContours = np.zeroes((w, h), np.uint8)
-    contours, hierarchy = cv2.findContours(img, cv2.RETER_EXTERNAL, cv2.CHAIN_APPROX_SIMP)
+    h, w = img.shape
+    imgContours = np.zeros((w, h), np.uint8)
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(img, contours, -1, (255, 0, 255), 2)
 
-    for cnt in contours:
-        pass
+    # for cnt in contours:
+    #     pass
 
     for i, cnt in enumerate(contours):
         area = cv2.contourArea(cnt)
@@ -43,6 +39,6 @@ def findContours(img):
             x, y, w, h = cv2.boundingRect(cnt)
             cv2.rectangle(imgContours, (x,y), (x+w, y+h), (0,255,0), 2)
 
-
+    print('Countours', imgContours)
     return imgContours
 
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import cv2
-from FindingBallons import*
+from FindingBallons import *
+from detectHit import detectHit
 
-img = cv2.imread("./test.jpg")
-
+img = cv2.imread('test.png')
+print('Shape', img.shape)
 imgBallons = findBallons(img)
-img = detectHit(img, bboxs)
+# print('hehe', imgBallons)
+img = detectHit(img, imgBallons)
 
 imgBallons = cv2.resize(img, (0,0), None, 0.5, 0.5)
 
